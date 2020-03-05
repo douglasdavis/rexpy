062180c09593a791126a643b62ef8a46e711dd52
Douglas Davis <ddavis@cern.ch>
2020-03-04 23:33:59 -0500

    ignore and new script

---
+++ scripts/rx-rank.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+import argparse
+import sys
+import os
+
+try:
+    from pathlib import PosixPath
+except ImportError:
+    try:
+        from pathlib2 import PosixPath
+    except ImportError:
+        try:
+            from WtPyext.pathlib import PosixPath
+        except ImportError:
+            print("cannot import PosixPath from anywhere; exiting")
+            exit(0)
+
+
+BNL_CONDOR_HEADER = """
+Universe        = vanilla
+notification    = Error
+notify_user     = ddavis@phy.duke.edu
+GetEnv          = True
+Executable      = {}
+Output          = logs/job.out.trexntup.$(cluster).$(process)
+Error           = logs/job.err.trexntup.$(cluster).$(process)
+Log             = /tmp/ddavis/log.$(cluster).$(process)
+request_memory  = 2.0G
+"""
+
+DESCRIPTION = (
+    "Generate a condor submission script which "
+    "performs the TRExFitter NP ranking work."
+)
+
+
+def get_args():
+    parser = argparse.ArgumentParser(description=DESCRIPTION)
+    parser.add_argument("config", type=str, help="TRExFitter config file")
+    parser.add_argument("outfile", type=str, help="output condor submit script")
+    return parser.parse_args()
+
+
+def main():
+    args = get_args()
+    full_config = str(PosixPath(args.config).resolve())
+    systematics, commands = [], []
+    with open(full_config, "r") as f:
+        for line in f.readlines():
+            if line.startswith(r"%"):
+                continue
+            elif line.startswith("Systematic:"):
+                s = line.strip().split("Systematic: ")[-1].replace('"', "")
+                systematics.append(s)
+    systematics = set(systematics)
+    for s in systematics:
+        commands.append("r {} Ranking={}".format(full_config, s))
+
+    with open(args.outfile, "w") as f:
+        exe = os.popen("which trex-fitter").read().strip()
+        print(BNL_CONDOR_HEADER.format(exe), file=f)
+        for com in commands:
+            print("Arguments = {}".format(com), file=f)
+            print("Queue\n", file=f)
+
+    PosixPath("logs").mkdir(exist_ok=True)
+
+
+if __name__ == "__main__":
+    main()
