518c3786711c6b59f76cd73221d3247627836847
Douglas Davis <ddavis@cern.ch>
2020-05-06 22:32:52 -0400

    python lib work

---
+++ rexuple/__init__.py
@@ -0,0 +1,7 @@
+def setup_logging():
+    import logging
+    logging.basicConfig(level=logging.INFO, format="{:25}  %(levelname)s  %(message)s".format("[%(name)s : %(funcName)s]"))
+    logging.addLevelName(logging.WARNING, "\033[1;31m{:8}\033[1;0m".format(logging.getLevelName(logging.WARNING)))
+    logging.addLevelName(logging.ERROR, "\033[1;35m{:8}\033[1;0m".format(logging.getLevelName(logging.ERROR)))
+    logging.addLevelName(logging.INFO, "\033[1;32m{:8}\033[1;0m".format(logging.getLevelName(logging.INFO)))
+    logging.addLevelName(logging.DEBUG, "\033[1;34m{:8}\033[1;0m".format(logging.getLevelName(logging.DEBUG)))
---
+++ rexuple/batch.py
@@ -0,0 +1,20 @@
+import os
+
+def job_params(workspace, executable, **kwargs):
+    universe = kwargs.get("universe", "vanilla")
+    getenv = kwargs.get("getenv", True)
+    notification = kwargs.get("notification", "Error")
+    request_memory = kwargs.get("request_memory", "2GB")
+    extra_lines = kwargs.get("extra_lines", ["email = ddavis@phy.duke.edu"])
+    return dict(
+        executable=executable,
+        universe=universe,
+        getenv=getenv,
+        notification=notification,
+        request_memory=request_memory,
+        extra_lines=extra_lines,
+        submit=os.path.join(workspace, "sub"),
+        error=os.path.join(workspace, "err"),
+        output=os.path.join(workspace, "out"),
+        log=os.path.join(workspace, "log")
+    )
