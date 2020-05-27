736d92eaa0122042e06123cb195301863a71fd5d
Douglas Davis <ddavis@cern.ch>
2020-05-27 13:37:06 -0400

    another rename commit

--- rp-init.sh
+++ rp-init.sh
@@ -11,11 +11,11 @@ else
     lsetup "root 6.18.04-x86_64-centos7-gcc8-opt" --quiet
 fi
 
-REXUPLE_DIR=$(realpath $(dirname "${BASH_SOURCE[0]}"))
-REXUPLE_SCRIPT_DIR=$REXUPLE_DIR/scripts
-REXUPLE_APP_DIR=$REXUPLE_DIR/app
+REXPY_DIR=$(realpath $(dirname "${BASH_SOURCE[0]}"))
+REXPY_SCRIPT_DIR=$REXPY_DIR/scripts
+REXPY_APP_DIR=$REXPY_DIR/app
 
-export PATH=$PATH:$REXUPLE_SCRIPT_DIR:$REXUPLE_APP_DIR
-export PATH=$PATH:$REXUPLE_SCRIPT_DIR/specific_studies
-export PATH=$PATH:$REXUPLE_SCRIPT_DIR/utilities
-export PYTHONPATH=$PYTHONPATH:$REXUPLE_DIR
+export PATH=$PATH:$REXPY_SCRIPT_DIR:$REXPY_APP_DIR
+export PATH=$PATH:$REXPY_SCRIPT_DIR/specific_studies
+export PATH=$PATH:$REXPY_SCRIPT_DIR/utilities
+export PYTHONPATH=$PYTHONPATH:$REXPY_DIR
