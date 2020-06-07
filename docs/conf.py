cecf4303006e3157683e2c3de9696f3ef9db62b7
Doug Davis <ddavis@ddavis.io>
2020-06-07 18:19:38 -0400

    docs

--- docs/conf.py
+++ docs/conf.py
@@ -76,10 +76,4 @@ html_theme_options = {
 intersphinx_mapping = {
     "python": ("https://docs.python.org/3", None),
     "numpy": ("https://docs.scipy.org/doc/numpy", None),
-    "dask": ("https://docs.dask.org/en/latest", None),
-    "uproot": ("https://uproot.readthedocs.io/en/latest", None),
-    "pandas": ("https://pandas.pydata.org/pandas-docs/stable", None),
-    "sklearn": ("https://scikit-learn.org/stable", None),
-    "lightgbm": ("https://lightgbm.readthedocs.io/en/latest", None),
-    "matplotlib": ("https://matplotlib.org", None),
 }
