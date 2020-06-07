7336e69ecd97d013b565ccf14588c14baff8f687
Doug Davis <ddavis@ddavis.io>
2020-06-07 18:28:13 -0400

    version won't be around for ATLAS only use

--- src/rexpy/__init__.py
+++ src/rexpy/__init__.py
@@ -1,14 +1,16 @@
-from .version import version
+try:
+    from .version import version
+except ImportError:
+    version = None
+    pass
 
 __version__ = version
 
+
 import logging
 
-def setup_logging():
-    logging.basicConfig(level=logging.INFO, format="{:25}  %(levelname)s  %(message)s".format("[%(name)s.%(funcName)s]"))
-    logging.addLevelName(logging.WARNING, "\033[1;31m{:8}\033[1;0m".format(logging.getLevelName(logging.WARNING)))
-    logging.addLevelName(logging.ERROR, "\033[1;35m{:8}\033[1;0m".format(logging.getLevelName(logging.ERROR)))
-    logging.addLevelName(logging.INFO, "\033[1;32m{:8}\033[1;0m".format(logging.getLevelName(logging.INFO)))
-    logging.addLevelName(logging.DEBUG, "\033[1;34m{:8}\033[1;0m".format(logging.getLevelName(logging.DEBUG)))
-
-setup_logging()
+logging.basicConfig(level=logging.INFO, format="{:25}  %(levelname)s  %(message)s".format("[%(name)s.%(funcName)s]"))
+logging.addLevelName(logging.WARNING, "\033[1;31m{:8}\033[1;0m".format(logging.getLevelName(logging.WARNING)))
+logging.addLevelName(logging.ERROR, "\033[1;35m{:8}\033[1;0m".format(logging.getLevelName(logging.ERROR)))
+logging.addLevelName(logging.INFO, "\033[1;32m{:8}\033[1;0m".format(logging.getLevelName(logging.INFO)))
+logging.addLevelName(logging.DEBUG, "\033[1;34m{:8}\033[1;0m".format(logging.getLevelName(logging.DEBUG)))
