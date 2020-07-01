1f1d2d391582d34cdf4f3d61c495c7f0a517d77c
Douglas Davis <ddavis@cern.ch>
2020-07-01 17:57:36 -0400

    new script to generate configs and condor jobs for sys stab tests

---
+++ src/scripts/utilities/rpu-gen-stab-tests.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+[[ -z "$1" ]] && echo "Config file required" && exit 1
+
+full_config=$1
+filename=$(basename -- "$full_config")
+extension="${filename##*.}"
+filename="${filename%.*}"
+trexexe=$(which trex-fitter)
+to_remove=(
+    ttbar_ptreweight_2j1b
+    ttbar_hdamp_2j2b
+    ttbar_PS_2j2b
+    ttbar_AR_FSR_2j1b
+    ttbar_AR_FSR_1j1b
+    tW_PS_2j1b
+    tW_PS_1j1b
+    tW_DRDS
+    MET_SoftTrk_Scale
+    Jet_Pileup_RhoTopology
+    Jet_JER_EffectiveNP_7restTerm
+    Jet_JER_EffNP_1
+    Jet_JER_DataVsMC
+    B_ev_B_0
+)
+
+[[ ! -d "rpcc_${filename}/tW/Histograms" ]] && "Existing histograms dir does not exist"
+
+for sys in "${to_remove[@]}"; do
+    newfile=$filename-without-$sys.$extension
+    rp-conf.py rm-sys $1 -s $sys -n $newfile
+    rp-condor.py complete $newfile --copy-histograms-from rpcc_${filename}/tW/Histograms --dont-draw --dont-rank
+done
