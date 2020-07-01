4bb88a86d7394f146412973ab17341cc8bc30f8c
Douglas Davis <ddavis@cern.ch>
2020-07-01 17:57:20 -0400

    new module for misc. helper functions

---
+++ src/rexpy/helpers.py
@@ -0,0 +1,27 @@
+def selection_with_period(raw, only_1516=False, only_17=False, only_18=False):
+    """Augment a selection to require a specific data taking period.
+
+    Parameters
+    ----------
+    raw : str
+        Raw selection string.
+    only_1516 : bool
+        Require 2015/2016
+    only_17 : bool
+        Require 2017
+    only_18 : bool
+        Require 2018
+
+    Returns
+    -------
+    str
+        Updated selection string
+    """
+    if only_1516:
+        return f"({raw}) && (isMC16a == 1)"
+    elif only_17:
+        return f"({raw}) && (isMC16d == 1)"
+    elif only_18:
+        return f"({raw}) && (isMC16e == 1)"
+    else:
+        return raw
