521f9544a31db29705c6193df83aa239bd81027d
Doug Davis <ddavis@ddavis.io>
2020-08-14 16:30:23 -0400

    add function to parse results to get list of compelely pruned systs.

--- src/rexpy/resparse.py
+++ src/rexpy/resparse.py
@@ -1,12 +1,13 @@
 import os
+from pathlib import Path
 
 
-def get_fit_parameter(workspace, fit_name="tW", parameter_name="SigXsecOverSM"):
+def get_fit_parameter(wkspace, fit_name="tW", parameter_name="SigXsecOverSM"):
     """Get a value from the fit.
 
     Parameters
     ----------
-    workspace : str
+    wkspace : str
         Path to the TRExFitter workspace.
     fit_name : str
         Name of the fit (the txt file is dependent on the fit name).
@@ -24,8 +25,8 @@ def get_fit_parameter(workspace, fit_name="tW", parameter_name="SigXsecOverSM"):
     float
         Down uncertainty
     """
-    fit_file = os.path.join(workspace, "Fits", "{}.txt".format(fit_name))
-    with open(fit_file, "r") as f:
+    fit_file = Path(wkspace) / "Fits" / f"{fit_name}.txt"
+    with fit_file.open("r") as f:
         for line in f.readlines():
             if parameter_name in line:
                 desired_line = line
@@ -35,3 +36,42 @@ def get_fit_parameter(workspace, fit_name="tW", parameter_name="SigXsecOverSM"):
     up = round(float(up), 5)
     down = round(float(down), 5)
     return name, central, up, down
+
+
+def completely_pruned(wkspace):
+    """Determine which systematics are completely pruned.
+
+    Parameters
+    ----------
+    wkspace : str or os.PathLike
+        Path of TRExFitter workspace.
+
+    Returns
+    -------
+    list(str)
+        Names of all completely pruned systematics.
+
+    """
+    pairs = []
+    with (Path(wkspace) / "PruningText.txt").open("r") as f:
+        for line in f:
+            if not line.startswith(" --->>"):
+                continue
+            sys, status = line.strip()[7:].split("      ")
+            if status == "is not present":
+                continue
+            pairs.append((sys, status))
+
+    unique = sorted(set([p[0] for p in pairs]))
+    tests = {u: 0 for u in unique}
+    for sys, status in pairs:
+        k = 0
+        if status == "is kept":
+            k = 1
+        elif status == "is shape only":
+            k = 1
+        elif status == "is norm only":
+            k = 1
+        tests[sys] += k
+
+    return [k for k, v in tests.items() if v == 0]
